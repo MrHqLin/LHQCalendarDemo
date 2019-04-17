709c4e0be6ad34b07fc1cf69fe5ae1fa9b9d6d3b
WaterWorld <lin@WaterWolddeiMac.lan>
2019-04-17 11:39:36 +0800

    first commit

---
+++ LHQCalendarDemo/Calendar/LHQCalendarCell.h
@@ -0,0 +1,33 @@
+//
+//  WTCalendarCell.h
+//  GeeLockApp
+//
+//  Created by water on 2018/8/28.
+//  Copyright © 2018年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LHQDateCell;
+
+@interface LHQWeekCell : UICollectionViewCell
+
+@property (nonatomic, strong) UILabel *weekLabel;
+
+@end
+
+@protocol WTDateCellDelegate <NSObject>
+
+- (void)clickDateWith:(LHQDateCell *)cell indexpath:(NSIndexPath *)indexpath;
+
+@end
+
+@interface LHQDateCell : UICollectionViewCell
+
+@property (nonatomic, weak) id<WTDateCellDelegate> cellDelegate;
+@property (nonatomic, strong) UIButton *dateButton;
+@property (nonatomic, strong) NSIndexPath *indexpath;
+
+@end
+
+
